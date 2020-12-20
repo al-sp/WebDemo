4737648311baa1b40b97fa0bcc84bbad66df88c1
al-sp <alexandr.spov@gmail.com>
2020-12-20 19:27:04 +0300

    api

---
+++ APIDemo/Controllers/FoodController.cs
@@ -0,0 +1,46 @@
+﻿using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IFoodData _foodData;
+
+        public FoodController(IFoodData foodData)
+        {
+            _foodData = foodData;
+        }
+
+        /*[HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get()
+        {
+            var food = await _foodData.GetFood();
+            
+            if (food != null)
+            {
+                return Ok(food);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }*/
+
+        [HttpGet]
+        public async Task<List<FoodModel>> Get()
+        {
+            return await _foodData.GetFood();
+        }
+    }
+}
